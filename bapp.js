e50ab0d6731bdaf900c1cd5364c5df61e62cc87d
Akash1321 <Sarkarakash992@gmail.com>
2022-08-30 14:00:51 +0530

    Baanana app done
    Completed the banana app

---
+++ bapp.js
@@ -0,0 +1,36 @@
+var btnClick = document.querySelector("#btn-click");
+var theText =document.querySelector("#text-area");
+var output = document.querySelector("#translated");
+
+var theURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function constructURL(text){
+    
+    return theURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("something wrong with the server try sometimes later")
+}
+
+btnClick.addEventListener("click", function eventHandler(){
+
+    var inputText = theText.value
+ 
+
+ fetch(constructURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        output.innerText = translatedText})
+    .catcn(errorHandler)
+
+    
+  
+     
+})
+
+
+    
